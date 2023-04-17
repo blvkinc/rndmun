6e5df3faccb1aed7776499d36603565e684bd198
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-04-17 02:45:06 -0700

    first commit

---
+++ p5js/sketch.js
@@ -0,0 +1,98 @@
+let song, fft;
+let cubes = [];
+let numCubes = 3;
+let cubeSize = 10;
+let angleX = 0;
+let angleY = 0;
+let cam;
+
+let minCamZ = 300;
+let maxCamZ = 1000;
+let camZ = 500;
+let isPlaying = true;
+
+function preload() {
+  song = loadSound('./1.wav');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  
+  fft = new p5.FFT();
+  
+  
+  for (let i = 0; i < numCubes; i++) {
+    for (let j = 0; j < numCubes; j++) {
+      let cube = new Cube(i * cubeSize - (numCubes / 2) * cubeSize, j * cubeSize - (numCubes / 2) * cubeSize, 0);
+      cubes.push(cube);
+    }
+  }
+
+  cam = createCamera();
+}
+
+function draw() {
+  background(0);
+  
+  // update camera position
+  camera(0, 0, camZ);
+  rotateX(angleX);
+  rotateY(angleY);
+ 
+
+  
+  let spectrum = fft.analyze();
+
+  for (let i = 0; i < numCubes * numCubes; i++) {
+    let cube = cubes[i];
+    let level = map(spectrum[i], 0, 255, 0, 4);
+    cube.update(level);
+    cube.display();
+  }
+}
+
+function mouseDragged() {
+  angleY += (mouseX - pmouseX) * 0.01;
+  angleX += (mouseY - pmouseY) * 0.01;
+}
+function mouseWheel(event) {
+  camZ += event.delta;
+  camZ = constrain(camZ, minCamZ, maxCamZ);
+}
+class Cube {
+  constructor(x, y, z) {
+    this.pos = createVector(x, y, z);
+    this.scale = createVector(1, 1, 1);
+  }
+
+  update(level) {
+    this.scale.z = map(level, 0, 1, 1, 2);
+  }
+
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y, this.pos.z);
+    scale(this.scale.x, this.scale.y, this.scale.z);
+    stroke(255);
+    strokeWeight(0.5);
+    fill(0);
+    box(cubeSize);
+    pop();
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    isPlaying = !isPlaying; 
+    if (isPlaying) {
+      song.play(); 
+    } else {
+      song.pause(); 
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+ 
---
+++ script.js
@@ -0,0 +1,87 @@
+
+$(document).ready(function() {
+  $('.shape').each(function(index) {
+    var title = $(this).data('title');
+    $(this).hover(function() {
+      $('#header h1').text(title);
+    }, function() {
+      $('#header h1').text('Portfolio');
+    });
+  });
+});
+
+$(document).ready(function() {
+  $('#back').click(function() {
+    window.location.href = '/index.html';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape1').click(function() {
+    window.location.href = 'pages/about_me.html';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape2').click(function() {
+    window.location.href = 'pages/my_work.html';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape3').click(function() {
+    window.location.href = 'pages/my_services.html';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape4').click(function() {
+    window.location.href = 'https://www.behance.net/dhanujasiriwar';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape5').click(function() {
+    window.location.href = 'https://www.instagram.com/blvk_inc/';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape6').click(function() {
+    window.location.href = 'https://www.linkedin.com/in/dhanuja-siriwardena/';
+  });
+});
+
+$(document).ready(function() {
+  $('#shape7').click(function() {
+    window.location.href = 'p5js/index.html';
+  });
+});
+
+
+
+
+
+
+
+
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+      return false;
+  }
+  if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))) {
+      return false;
+  }
+  if (e.ctrlKey && e.shiftKey && (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))) {
+      return false;
+  }
+  if (e.ctrlKey && e.shiftKey && (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))) {
+      return false;
+  }
+  if (e.ctrlKey && (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))) {
+      return false;
+  }
+  if (e.ctrlKey && (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))) {
+      return false;
+  }
+}
