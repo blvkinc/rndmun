404abb562bacc9a4e58d3605d3afffa79e326868
Dhanuja Siriwardena <40000010+blvkinc@users.noreply.github.com>
2023-04-17 09:31:56 -0700

    Update sketch.js

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,98 +1,78 @@
-let song, fft;
-let cubes = [];
-let numCubes = 3;
-let cubeSize = 10;
-let angleX = 0;
-let angleY = 0;
-let cam;
+let darkMode = true
 
-let minCamZ = 300;
-let maxCamZ = 1000;
-let camZ = 500;
-let isPlaying = true;
+let capture
 
-function preload() {
-  song = loadSound('/rndmun/p5js/1.mp3');
+let bg, tc, newScreen
+
+const font = 'monospace'
+const font_ratio = 0.5
+
+let charRamp = " .:-=+*#%@"
+//"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+const density = 25; //how many pixels go into one character
+
+function reverseStr(str){
+  rev = ''
+  for (let l = str.length - 1; l >= 0; l-= 1) {
+    rev += str[l]
+  }
+  return rev
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  
-  fft = new p5.FFT();
-  
-  
-  for (let i = 0; i < numCubes; i++) {
-    for (let j = 0; j < numCubes; j++) {
-      let cube = new Cube(i * cubeSize - (numCubes / 2) * cubeSize, j * cubeSize - (numCubes / 2) * cubeSize, 0);
-      cubes.push(cube);
-    }
+  if (darkMode) {
+    bg = color(10, 10, 20)
+    tc = color(255)
+    charRamp = reverseStr(charRamp)
+  } else {
+    bg = color(255)
+    tc = color(0)
   }
 
-  cam = createCamera();
+  capture = createCapture(VIDEO);
+  capture.size(windowHeight, windowWidth);
+  width = windowWidth;
+  height = windowHeight;
+  createCanvas(width, height);
+  screen = createGraphics(width, height)
+  newScreen = createGraphics(int(screen.width / density / font_ratio), int(screen.height / density))
+  
+  screen.fill(0)
+  textFont(font, density)
+  fill(tc)
+  noStroke()
+  textSize(density)
+  textLeading(density)
+  textAlign(LEFT, TOP)
 }
 
 function draw() {
-  background(0);
+  background(bg);
+  screen.image(capture, 0, 0, screen.width, screen.height)
+  updateAscii()
   
-  // update camera position
-  camera(0, 0, camZ);
-  rotateX(angleX);
-  rotateY(angleY);
- 
-
-  
-  let spectrum = fft.analyze();
-
-  for (let i = 0; i < numCubes * numCubes; i++) {
-    let cube = cubes[i];
-    let level = map(spectrum[i], 0, 255, 0, 4);
-    cube.update(level);
-    cube.display();
+  if (keyIsPressed) {
+    image(screen, 0, 0, width, height)
   }
 }
 
-function mouseDragged() {
-  angleY += (mouseX - pmouseX) * 0.01;
-  angleX += (mouseY - pmouseY) * 0.01;
-}
-function mouseWheel(event) {
-  camZ += event.delta;
-  camZ = constrain(camZ, minCamZ, maxCamZ);
-}
-class Cube {
-  constructor(x, y, z) {
-    this.pos = createVector(x, y, z);
-    this.scale = createVector(1, 1, 1);
-  }
+function updateAscii() {
+  const inv_max = 1/(255*3*256)
+  newScreen.clear()
+  newScreen.image(screen, 0, 0, newScreen.width, newScreen.height)
 
-  update(level) {
-    this.scale.z = map(level, 0, 1, 1, 2);
-  }
-
-  display() {
-    push();
-    translate(this.pos.x, this.pos.y, this.pos.z);
-    scale(this.scale.x, this.scale.y, this.scale.z);
-    stroke(255);
-    strokeWeight(0.5);
-    fill(0);
-    box(cubeSize);
-    pop();
-  }
-}
-
-function keyPressed() {
-  if (keyCode === 32) {
-    isPlaying = !isPlaying; 
-    if (isPlaying) {
-      song.play(); 
-    } else {
-      song.pause(); 
+  newScreen.loadPixels()
+  newText = ''
+  const pxs = newScreen.pixels
+  for (let y = 0; y < newScreen.height; y++) {
+    for (let x = 0; x < newScreen.width; x++) {
+      const i = ((y*newScreen.width) + x)*4
+      br = (pxs[i] + pxs[i + 1] + pxs[i + 2])*pxs[i + 3]
+      pixChar = charRamp[int(br*inv_max*charRamp.length)]
+      newText += pixChar
     }
+    newText = newText + '\n'
   }
+  text(newText, 0, 0)
 }
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
- 
